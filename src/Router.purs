1cf23cd1ccf4f1a110c6f4c6183ff8e972091160
Willis Plummer <willisplummer@gmail.com>
2019-03-02 12:14:28 -0500

    almost working json response

--- src/Router.purs
+++ src/Router.purs
@@ -1,6 +1,39 @@
 module Router where
 
+import Prelude (($))
 import HTTPure as HTTPure
+import Data.Argonaut (class EncodeJson, encodeJson, jsonEmptyObject, (:=), (~>))
+import Data.Argonaut.Core (toString)
+import Data.Maybe (Maybe(..))
+
+newtype Judgeable = Judgeable {
+  id :: Int
+, name :: String
+, imageUrl :: String
+}
+
+banana :: Judgeable
+banana = Judgeable {
+  id: 1
+, name: "banana"
+, imageUrl: "fakeUrl"
+}
+
+instance encodeJsonJudgeable :: EncodeJson Judgeable where
+  encodeJson (Judgeable thing)
+     = "id" := thing.id
+    ~> "name" := thing.name
+    ~> "imageUrl" := thing.imageUrl
+    ~> jsonEmptyObject
+
+getJudgeableThingHandler :: HTTPure.ResponseM
+getJudgeableThingHandler =
+  case maybeResponseString of
+    (Just stringifiedJson) -> HTTPure.ok stringifiedJson
+    Nothing -> HTTPure.internalServerError "something went wrong"
+  where maybeResponseString = toString $ encodeJson banana
 
 router :: HTTPure.Request -> HTTPure.ResponseM
-router {} = HTTPure.ok "hello world!"
+router { body, path: [ "things" ], method: HTTPure.Post }  = HTTPure.ok body
+router { body, path: [ "things", "new" ], method: HTTPure.Post } = HTTPure.ok body
+router {} = getJudgeableThingHandler
